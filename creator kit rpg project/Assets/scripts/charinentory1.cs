00b94a194e86f97f9031fd359995484779d78b6b
dtapp935 <56683367+dtapp935@users.noreply.github.com>
2019-10-29 10:20:36 -0400

    update

---
+++ creator kit rpg project/Assets/scripts/charinentory1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charinentory1 : MonoBehaviour
+{
+    //Declare Variables
+    public int appleCount = 0;
+    public GameObject crate;
+
+    //when collide with trigger
+    //if the other thing is tagged "apple"
+    //then add 1 to our appleCount
+    //and if appleCount is > x, Crate. Setactive(false)
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if((other.gameObject.CompareTag"apple"))
+
+            Debug.Log("you got an apple");
+        appleCount += 1;
+        if(appleCount >= 2)
+        {
+
+            crate.SetActive(false) 
+        }
+    }
+}
